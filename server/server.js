b75ce82075bf9cbe64a71e36bf89fe643e98b957
LexieHolder <lexieannholder@gmail.com>
2022-03-25 18:15:38 -0400

    exchanged appollo into server side

--- server/server.js
+++ server/server.js
@@ -1,21 +1,57 @@
+//similar to what we used in Poke-Buff to import apollo and apply to express server as middleware, made changes to fit
+
+//require express
 const express = require('express');
+
+// import authentication middleware
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  // create an Apollo server and pass in schema info
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context: authMiddleware
+  });
+
+  // start apollo server
+  await server.start();
+
+  // integrate our apollo server with the express application as middleware
+  server.applyMiddleware({ app });
+
+  // test GraphQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,53 @@
-const jwt = require('jsonwebtoken');
+import decode from 'jwt-decode';
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
-
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+class AuthService {
+    // retrieve data saved in token
+    getProfile() {
+      return decode(this.getToken());
     }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+  
+    // check if the user is still logged in
+    loggedIn() {
+      // Check for a saved token and if it's still valid
+      const token = this.getToken();
+      // use type coersion to check if token is NOT undefined and the token is NOT expired
+      return !!token && !this.isTokenExpired(token);
     }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+  
+    // check if the token has expired
+    isTokenExpired(token) {
+      try {
+        const decoded = decode(token);
+        if (decoded.exp < Date.now() / 1000) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (err) {
+        return false;
+      }
     }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+  
+    // retrieve token from localStorage
+    getToken() {
+      // Retrieves the user token from localStorage
+      return localStorage.getItem('id_token');
+    }
+  
+    // set token to localStorage and reload page to homepage
+    login(idToken) {
+      // save user token to localStorage
+      localStorage.setItem('id_token', idToken);
+  
+      window.location.assign('/');
+    }
+  
+    // clear token from localStorage and force logout with reload
+    logout() {
+      // clear user token and profile data from localStorage
+      localStorage.removeItem('id_token');
+      // reload page and reset state of application
+      window.location.assign('/');
+    }
+  }
+export default new AuthService();
